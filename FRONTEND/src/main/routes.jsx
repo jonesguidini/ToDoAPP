d8c6bf5e382767613b59c2eb83479b56b27eb709
Jones Guidini <jonesguidini@hotmail.com>
2020-03-03 11:05:32 -0300

    fix: ajustes gerais

--- FRONTEND/src/main/routes.jsx
+++ FRONTEND/src/main/routes.jsx
@@ -8,7 +8,7 @@ export default props => (
     <Switch>
         <Route path="/todos" component={Todo} />
         <Route path="/about" component={About} />
-        <Redirect from="*" to='/todos' />
+        <Route path="/" exact component={Todo} />
     </Switch>
 
 )
