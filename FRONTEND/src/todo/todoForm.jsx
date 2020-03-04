a31722a7e1012ace5c408d399fa4ee757121f07a
Jones Guidini <jonesguidini@hotmail.com>
2020-03-03 23:26:26 -0300

    feat: adição template grid + helper if + template button

--- FRONTEND/src/todo/todoForm.jsx
+++ FRONTEND/src/todo/todoForm.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import Grid from "../template/grid";
+import Iconbutton from "../template/iconbutton";
 
 export default props => (
   <div role="form" className="todoForm">
     <div className="row">
-      <div className="col-xs-12 col-sm-9 col-md-10">
+      <Grid cols="12 9 10">
         <input
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"
         />
-      </div>
+      </Grid>
 
-      <div className="col-xs-12 col-sm-3 col-md-2">
-        <button className="btn btn-primary">
-          <i className="fa fa-plus"></i>
-        </button>
-      </div>
+      <Grid cols="12 3 2">
+        <Iconbutton style="primary" icon="plus"></Iconbutton>
+      </Grid>
     </div>
   </div>
 );
