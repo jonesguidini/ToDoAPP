a3917baf657c20022bb17f6d77d820a0aff7d38c
Jones Guidini <jonesguidini@hotmail.com>
2020-03-09 13:42:00 -0300

    fix: other fixes

--- FRONTEND/src/todo/todoList.jsx
+++ FRONTEND/src/todo/todoList.jsx
@@ -11,33 +11,44 @@ export default props => {
         <tr><td className="text-center" colSpan="2">Nenhum registro encontrado</td></tr>
       )
     }
+
+
       
-    return list.map(todo => (
-      <tr key={todo.Id}>
-        <td className={todo.IsDone ? "table-warning" : ""}>{todo.Title}</td>
-        <td
-          className={todo.IsDone ? "table-warning text-center" : "text-center"}
-        >
-          <IconButton
-            style="success"
-            hide={todo.IsDone}
-            icon="check"
-            onClick={() => props.handleUpdateStatus(todo)}
-          ></IconButton>
-          <IconButton
-            style="warning"
-            hide={!todo.IsDone}
-            icon="undo"
-            onClick={() => props.handleUpdateStatus(todo)}
-          ></IconButton>
-          <IconButton
-            style="danger"
-            icon="trash-o"
-            onClick={() => props.handleRemove(todo)}
-          ></IconButton>
-        </td>
-      </tr>
-    ));
+    return list.map(todo => {
+
+      let classTitle = 'align-middle '
+      classTitle += todo.IsDone ? "table-warning" : classTitle
+  
+      let classButtons = 'text-center '
+      classButtons += todo.IsDone ? "table-warning " : classButtons
+
+      return (
+          <tr key={todo.Id}>
+            <td className={classTitle}>{todo.Title}</td>
+            <td
+              className={classButtons}
+            >
+              <IconButton
+                style="success"
+                hide={todo.IsDone}
+                icon="check"
+                onClick={() => props.handleUpdateStatus(todo)}
+              ></IconButton>
+              <IconButton
+                style="warning"
+                hide={!todo.IsDone}
+                icon="undo"
+                onClick={() => props.handleUpdateStatus(todo)}
+              ></IconButton>
+              <IconButton
+                style="danger"
+                icon="trash-o"
+                onClick={() => props.handleRemove(todo)}
+              ></IconButton>
+            </td>
+          </tr>
+        )}
+    );
   };
 
   return (
